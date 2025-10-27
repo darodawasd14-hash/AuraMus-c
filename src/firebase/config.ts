49c1c04a98e241e312a70a163798773294bc1501
Oguzhan Arman <oguzhanarman01@gmail.com>
2025-10-27 16:06:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7673683321-c816a",
+  "appId": "1:490835418667:web:8e1f91310894fbccae946e",
+  "apiKey": "AIzaSyApOJYmEhgqSTh6OD2uVgI9kSvXqdo9Hok",
+  "authDomain": "studio-7673683321-c816a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "490835418667"
+};
