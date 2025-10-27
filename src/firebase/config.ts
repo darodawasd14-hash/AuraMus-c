4e61f5c36e96a77ebf7b486aba26ab420aff8344
Oguzhan Arman <oguzhanarman01@gmail.com>
2025-10-27 21:18:33 +0000

    Google Analytics SDK'sını ekleyin ekledim ekle bakalım bizim uygulamaya

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:490835418667:web:8e1f91310894fbccae946e",
   "apiKey": "AIzaSyApOJYmEhgqSTh6OD2uVgI9kSvXqdo9Hok",
   "authDomain": "studio-7673683321-c816a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "490835418667"
+  "measurementId": "G-M9C53VZS8Y",
+  "messagingSenderId": "490835418667",
+  "storageBucket": "studio-7673683321-c816a.appspot.com"
 };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,6 +4,7 @@ import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics, Analytics } from "firebase/analytics";
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -33,10 +34,16 @@ export function initializeFirebase() {
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
+  let analytics;
+  if (typeof window !== 'undefined') {
+    analytics = getAnalytics(firebaseApp);
+  }
+
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: getFirestore(firebaseApp),
+    analytics,
   };
 }
 
