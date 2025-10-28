bbd3a54572c98ba6a89ad44b9616bd633f069de0
Oguzhan Arman <oguzhanarman01@gmail.com>
2025-10-28 17:06:56 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  // In a future major version of Next.js, you will need to explicitly configure "allowedDevOrigins"
+  // to allow cross origin requests to /_next/* resources.
+  // Read more: https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
+  allowedDevOrigins: [
+    'https://*.cluster-jbb3mjctu5cbgsi6hwq6u4btwe.cloudworkstations.dev',
+  ],
   images: {
     remotePatterns: [
       {
