c8abd6e7d4b130fe07b1b00803031cd3ab0e8073
Oguzhan Arman <oguzhanarman01@gmail.com>
2025-10-30 23:52:16 +0000

    Firebase config'i Vercel degiskenlerini okuyacak sekilde guncelledim

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-7673683321-c816a",
-  "appId": "1:490835418667:web:8e1f91310894fbccae946e",
-  "apiKey": "AIzaSyApOJYmEhgqSTh6OD2uVgI9kSvXqdo9Hok",
-  "authDomain": "studio-7673683321-c816a.firebaseapp.com",
-  "measurementId": "G-M9C53VZS8Y",
-  "messagingSenderId": "490835418667",
-  "storageBucket": "studio-7673683321-c816a.appspot.com"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
