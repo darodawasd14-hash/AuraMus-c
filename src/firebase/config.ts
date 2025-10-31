836f5c6e88375e13b484df1c1cd1a54e72f7666e
Oguzhan Arman <oguzhanarman01@gmail.com>
2025-10-31 00:05:43 +0000

    Son config duzeltmesi (Hem Vercel hem Studio)

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
+// Hem Vercel'de (process.env) hem de Studio'da (|| sonrakiler) çalışır
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyApOJYmEhgqSTh6OD2uVgI9kSvXqdo9Hok",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-7673683321-c816a.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-7673683321-c816a",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-7673683321-c816a.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "490835418667",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:490835418667:web:8e1f91310894fbccae946e",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-M9C53VZS8Y"
 };
